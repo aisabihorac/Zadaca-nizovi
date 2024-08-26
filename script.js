0af0818b68b0ef5862c78e56a7fcfc1742604d32
aisa <aisabihorac965@gmail.com>
2024-08-26 22:55:00 +0200

    Zadatak 2  - Pronadi negativne 1 bod

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ function doubleNumbers(brojevi) {
   return brojevi.map((broj) => broj * 2);
 }
 console.log(doubleNumbers(brojevi));
+
+//Drugi zadatak
+let niz = [4, 76, -446, -4];
+function findNegatives(niz) {
+    return niz.filter(broj => broj < 0);
+
+
+}
+console.log(findNegatives(niz));
